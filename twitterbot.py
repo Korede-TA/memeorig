9c624d865ebadb9bf4dad25c56431779a82ccd02
Kathrina <kathrinawaugh@gmail.com>
2018-03-24 03:57:58 -0400

    tweetbot added

---
+++ twitterbot.py
@@ -0,0 +1,51 @@
+import tweepy
+import time
+import datetime
+
+consumerKey = 'TNRHGsKZ9eLFicSVp0mYz8jES'
+consumerSecret = 'pAJME2uhBnc3cJlcTsFfNLtnoPWos0l9YiRLhiNxJxBGtH1YlR'
+accessToken = '977428488398483456-IpIS5IqEsVJRGyrqoWOBom9AlKVg6WL'
+accessTokenSecret = 'NkMVr8B0g7ehSQNbX8WUOymTIuN7wFng5CAQe4QMZR1ox'
+
+auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
+auth.set_access_token(accessToken, accessTokenSecret)
+
+auth.secure = True
+api = tweepy.API(auth)
+
+
+for tweet in tweepy.Cursor(api.search, q = '#car', since = '2018-03-23', until = '2018-03-24', lang = "en").items():
+
+    try:
+        print("\n")
+        print("Found tweet by: @" + tweet.user.screen_name)
+
+        if (tweet.retweeted == False):
+            tweet.retweet()
+            print("Retweeted the tweet")
+        else:
+            print("Already retweeted the tweet")
+        if (tweet.favorited == False):
+            tweet.favorite()
+            print("Favorited the tweet")
+        else:
+            print("Already favorited the tweet")
+        if tweet.user.following == False:
+            tweet.user.follow()
+            print("Followed the user")
+        else:
+            print("Already Following the user")
+
+    except tweepy.TweepError as e:
+            print (e.reason)
+            #time.sleep(10)
+            continue
+
+    except StopIteration:
+        break
+
+
+
+
+
+
